40b33cb0a9195687c4a7deda37e4f1d32e8a6e0d
kogo198 <nehemiahkipagat@gmail.com>
2025-10-07 21:54:10 +0300

    Update exam attended.c

--- exam attended.c
+++ exam attended.c
@@ -3,24 +3,27 @@ name:NEHEMIAH KIPLAGAT
 REG:PA106/G/28723/25
 DISCRIPTION:a program that checks student eligible for final exam
 */
-#include <stdio.h>
-int main() {
-    int attendance, average;
-    
-    // Input attendance and avarage marks
-    printf("Enter number of classes attended : ");
-    scanf("%d", &attendance);
-    printf("Enter number you avarage marks: ");
-    scanf("%d", &average);
-    
-    // Check eligibility
-    if (attendance >= 70% && avarage  >= 40) {
-        printf("Eligible for final exam.\n");
+ #include <stdio.h>
+int main(){
+    float attendance, avg_marks;
+    //attendance percentage
+    printf("Enter attendance percentage: ");
+    scanf("%f", &attendance);
+    //avarage marks
+    printf("Enter average marks: ");
+    scanf("%f", &avg_marks);
+    //condition
+    if(attendance >= 75 && avg_marks >= 40){
+        printf("Eligible for final exams.\n");
     } else {
-        printf("Not eligible for final exam.\n");
+        printf("Not eligible.\n");
     }
+    return 0;
+
+}
     
     return 0;
 }
+
 
 
