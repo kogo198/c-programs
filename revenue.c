a022795706059743cdc1fde0ace722fe9f95c46d
kogo198 <nehemiahkipagat@gmail.com>
2025-10-26 17:24:52 +0300

    Add files via upload

---
+++ revenue.c
@@ -0,0 +1,32 @@
+/*
+NAME:NEHEMIAH KIPLAGAT
+REG:PA106/G/28723/25
+DESCRIPTION:THIS IS 1D FOR REVENUE IN ONE WEEK
+*/
+#include <stdio.h>
+
+int main() {
+    int i;
+    int sum = 0;
+    int revenue [7]; 
+	int avarage_revenue; 
+
+    // Ask user to input the revenue
+    printf("Enter 7 revenue:\n");
+    for (i = 0; i < 7; i++) {
+        printf("revenue %d: ", i + 1);
+        scanf("%d", &revenue[i]);
+    }
+
+    // Display and calculate the sum
+    printf("\nYou entered:\n");
+    for (i = 0; i < 7; i++) {
+        printf("%d\n", revenue[i]);
+        sum += revenue[i];
+    }
+    avarage_revenue=sum/7 ;
+
+    printf("The sum is %d\n", sum);
+printf("the avarage_revenue is %d\n",avarage_revenue);
+    return 0;
+}
