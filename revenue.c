ec225b5b3b210a5caa4642f5294f82c82bb914be
kogo198 <nehemiahkipagat@gmail.com>
2025-10-26 17:51:54 +0300

    Refactor revenue calculation and improve comments
    Fixed average revenue calculation and corrected comments.

--- revenue.c
+++ revenue.c
@@ -1,17 +1,17 @@
 /*
 NAME:NEHEMIAH KIPLAGAT
 REG:PA106/G/28723/25
-DESCRIPTION:THIS IS 1D FOR REVENUE IN ONE WEEK
+DISCRIPTION:A SIMPLE 1D C CODE FOR CALCULATING AND DOING AVARAGE REVENUE
 */
 #include <stdio.h>
 
 int main() {
     int i;
     int sum = 0;
-    int revenue [7]; 
-	int avarage_revenue; 
+    int revenue[7];
+	int avarage_revenue
 
-    // Ask user to input the revenue
+    // Ask user to input the numbers
     printf("Enter 7 revenue:\n");
     for (i = 0; i < 7; i++) {
         printf("revenue %d: ", i + 1);
@@ -24,9 +24,10 @@ int main() {
         printf("%d\n", revenue[i]);
         sum += revenue[i];
     }
-    avarage_revenue=sum/7 ;
+	avarage_revenue=sum/7
 
     printf("The sum is %d\n", sum);
-printf("the avarage_revenue is %d\n",avarage_revenue);
+	printf("avarage_revenue is %d\n",avarage_revenue);
+
     return 0;
 }
