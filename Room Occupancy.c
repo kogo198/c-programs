e19b6ce23ba4cef87f23fd828d7d1fc43099710c
kogo198 <nehemiahkipagat@gmail.com>
2025-10-29 14:14:43 +0300

    Add files via upload

---
+++ Room Occupancy.c
@@ -0,0 +1,29 @@
+/*
+NAME:NEHEMIAH KIPLAGAT
+REG:PA106/G/28723/25
+DISCRIPTION:A SIMPLE 2D C CODE FOR ROOM OCCUPACTION
+*/
+#include <stdio.h>
+int main(){
+	int i,j;
+	int occupancy[5][10];
+	 printf("Enter 7 revenue:\n");
+    for (i = 0; i < 5; i++) {
+    	for(j=0;j<10;j++){
+        printf("occupancy %d: ", i + 1);
+        scanf("%d", &occupancy[i][j]);
+		}
+	
+	}
+        
+	for (i=0;i<5;i++){
+		for (j=0;j<10;j++){
+			printf("%d \t",occupancy[i][j]);
+		}
+		printf("\n");
+	}
+     
+	
+	
+	return 0;
+}
